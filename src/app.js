900bf5758a5dd8462b05d922a784b26806772d0f
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2022-07-17 13:22:55 +0100

    compress method algorithm

--- src/app.js
+++ src/app.js
@@ -47,3 +47,27 @@ const uncompress = (s) => {
     return result.join('');
   };
   
+
+  // this operation is the most efficient way
+  const compress = (s) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    
+    while (j <= s.length) {
+      if (s[i] === s[j]) {
+        j += 1;
+      } else {
+        const num = j - i;
+        if (num === 1) {
+          result.push(s[i]);
+        } else {
+            //for a linear operation
+          result.push(String(num), s[i]);
+        }
+        i = j;
+      }
+    }
+    // and also a linear operation too
+    return result.join('');
+  }; 
